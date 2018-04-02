0efb8822d8a0e27e66f469d15bd984286aa068d6
opl- <opl-@users.noreply.github.com>
2018-04-02 22:14:53 +0200

    Changed for actual code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const config = require('./config');
-const RedditSnakeDump = require('./lib/RedditSnakeDump');
+const RedditCircleDump = require('./lib/RedditCircleDump');
 
-new RedditSnakeDump(config);
+new RedditCircleDump(config);
